bbf006266998e2091b47a2e572d8a85381246361
guillermo <gblasco@bloobirds.com>
2020-12-14 20:06:15 +0100

    feat(cors): added cors config to allow all origins

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ require("express-async-errors")
 const errorHandler = require("./middlewares/error-handler")
 const RecipeController = require("./controllers/recipe-controller")
 const fetch = require("node-fetch")
+const cors = require('cors');
 
 const app = express()
 const port = process.env.PORT || 8000
 
+app.use(cors( {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}))
 app.use(express.json())
 app.use("/recipes", RecipeController)
 app.use(errorHandler)
